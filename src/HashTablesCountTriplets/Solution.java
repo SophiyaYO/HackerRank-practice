cc8175cb15d0f2faae3b4cccf8a48da0a86450c3
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-17 10:14:27 +0300

    initial commit

---
+++ src/HashTablesCountTriplets/Solution.java
@@ -0,0 +1,45 @@
+package HashTablesCountTriplets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the countTriplets function below.
+    static long countTriplets(List<Long> arr, long r) {
+        long counter = 0;
+
+        return counter;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nr[0]);
+
+        long r = Long.parseLong(nr[1]);
+
+        List<Long> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long ans = countTriplets(arr, r);
+
+        System.out.println(ans);
+
+//        bufferedWriter.write(String.valueOf(ans));
+//        bufferedWriter.newLine();
+
+        bufferedReader.close();
+//        bufferedWriter.close();
+    }
+}
+
